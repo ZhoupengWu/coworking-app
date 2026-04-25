f50e406bfa2a6939a872cad0b6a5a5555eb793df
ZhoupengWu <182263202+ZhoupengWu@users.noreply.github.com>
2026-04-25 21:40:36 +0200

    Seed data

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"coworkingApp/handlers"
 	"coworkingApp/models"
+	"coworkingApp/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
@@ -22,6 +23,7 @@ func main () {
 	db.AutoMigrate(&models.Room{})
 	db.AutoMigrate(&models.Photo{})
 	db.AutoMigrate(&models.Booking{})
+	seedData(db)
 	r := gin.Default()
 
 	r.GET("/rooms", handlers.GetAllRooms)
@@ -35,4 +37,78 @@ func main () {
 	if err := r.Run(":8080"); err != nil {
 		panic(err)
 	}
+}
+
+func seedData (db *gorm.DB) {
+	db.Delete(&models.User{}, "1 = 1")
+	db.Delete(&models.Room{}, "1 = 1")
+	db.Delete(&models.Photo{}, "1 = 1")
+	db.Delete(&models.Booking{}, "1 = 1")
+
+	userID := utils.GetUUID()
+	db.Create(&models.User{
+		ID: userID,
+		Email: "wux@wux.com",
+		Username: "wux",
+		Password: "wux1xuw9",
+	})
+	db.Create([]*models.Room{
+		{
+			ID: utils.GetUUID(),
+			Name: "Green",
+			Cost: 99.99,
+			NumberOfSeats: 6,
+			Category: "Spazio di piacere",
+			MainPhoto: "/green_0001.png",
+			Photos: []models.Photo{
+				{
+					Url: "/green_0002.png",
+				},
+				{
+					Url: "/green_0003.png",
+				},
+				{
+					Url: "/green_0004.png",
+				},
+			},
+		},
+		{
+			ID: utils.GetUUID(),
+			Name: "Red",
+			Cost: 599.99,
+			NumberOfSeats: 150,
+			Category: "Sala conferenza",
+			MainPhoto: "/red_0001.png",
+			Photos: []models.Photo{
+				{
+					Url: "/red_0002.png",
+				},
+				{
+					Url: "/red_0003.png",
+				},
+				{
+					Url: "/red_0004.png",
+				},
+			},
+		},
+		{
+			ID: utils.GetUUID(),
+			Name: "Yellow",
+			Cost: 299.99,
+			NumberOfSeats: 50,
+			Category: "Aula di apprendimento",
+			MainPhoto: "/yellow_0001.png",
+			Photos: []models.Photo{
+				{
+					Url: "/yellow_0002.png",
+				},
+				{
+					Url: "/yellow_0003.png",
+				},
+				{
+					Url: "/yellow_0004.png",
+				},
+			},
+		},
+	})
 }
